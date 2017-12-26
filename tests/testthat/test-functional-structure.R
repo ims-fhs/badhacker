eb7ad4a2b778fc2793a148fc1f8b0c802586a69c
Christoph Strauss <christoph.strauss@fhsg.ch>
2017-12-26 21:46:27 +0100

    Add tests for basic functions. Design of create_list_of_functional_structure() not really good. See test-chaos in tests for basic functions. All tests running.

--- tests/testthat/test-functional-structure.R
+++ tests/testthat/test-functional-structure.R
@@ -1,6 +1,33 @@
 context("basic functions")
 test_that("test basic functions", {
 
+  lines <- readLines(paste0(path, filename), encoding = "UTF-8")
+  open_braces <- calculate_number_of_open_braces(lines)
+  testthat::expect_equal(open_braces, c(0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 0,
+                                        0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1,
+                                        1, 0, 0, 0, 0, 0))
+
+  codelines_with_function_names <- strsplit(lines[grepl(" <- function", lines)],
+                                            " <- function", fixed = T)
+  function_arguments <- unlist(lapply(c(1:length(codelines_with_function_names)), function(x)
+    gsub(" +|(\\()|(\\))|(\\{)", "", codelines_with_function_names[[x]][2])))
+  list_of_function_arguments <- strsplit(function_arguments, ",")
+
+  r <- get_variables_defaults(list_of_function_arguments, 1)
+  testthat::expect_equal(r$names, "")
+  testthat::expect_equal(r$defaults, "")
+
+  r <- get_variables_defaults(list_of_function_arguments, 2)
+  testthat::expect_equal(r$names, c("x", "y", "a"))
+  testthat::expect_true(all(is.na(r$defaults)))
+
+  r <- get_variables_defaults(list_of_function_arguments, 3)
+  testthat::expect_equal(r$names, c("x", "y", "b", "c"))
+  testthat::expect_equal(r$defaults, c(NA, NA, "0", "4"))
+})
+
+context("functional_structure")
+test_that("test create_list_of_functional_structure", {
   my_structure <- create_list_of_functional_structure(filename, path)
 
   testthat::expect_equal(length(my_structure), 5)
